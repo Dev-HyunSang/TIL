fd17ffb30972e8c3f0d241ae2d288858d8fe6597
HyunSang Lev Park <me@hyunsang.dev>
2025-12-19 19:56:10 +0900

    fix: 도메인 설정 오류 발생, 재수정.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
   lang: "en-US",
-  base: "til.hyunsang.dev/",
+  base: "/",
   title: "HyunSang's Today I Learned",
   description: "Cyber Security, Software Engineering, and Law",
 
